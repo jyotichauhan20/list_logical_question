87eb2e938f5def1dc6ddd9f37d49f9dbedd77c5f
jyotichauhan20 <72217749+jyotichauhan20@users.noreply.github.com>
2021-01-04 16:48:34 +0530

    Add files via upload

---
+++ code2.py
@@ -0,0 +1,7 @@
+list=["rubi","rubina","rabina","rabiya","jyoti","papa","pappu"]
+string=input("enter your string=")
+i=0
+while i<len(list):
+    if list[i][0]==string:
+        print(list[i])
+    i=i+1
